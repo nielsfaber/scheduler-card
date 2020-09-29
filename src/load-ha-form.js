7acded32f4155901ed6d99ad3240e930cdf46f1f
Kendell R <KTibow@users.noreply.github.com>
2020-09-29 08:57:05 -0700

    ESLint fixes

--- src/load-ha-form.js
+++ src/load-ha-form.js
@@ -1,19 +1,20 @@
-
 export const loadHaForm = async () => {
-  if (customElements.get("ha-checkbox") && customElements.get("ha-paper-slider")) return;
+  if (customElements.get('ha-checkbox') && customElements.get('ha-paper-slider')) return;
 
-  await customElements.whenDefined("partial-panel-resolver");
+  await customElements.whenDefined('partial-panel-resolver');
   const ppr = document.createElement('partial-panel-resolver');
   ppr.hass = {
-    panels: [{
-      url_path: "tmp",
-      component_name: "config",
-    }]
+    panels: [
+      {
+        url_path: 'tmp',
+        component_name: 'config',
+      },
+    ],
   };
   ppr._updateRoutes();
   await ppr.routerOptions.routes.tmp.load();
 
-  await customElements.whenDefined("ha-panel-config");
-  const cpr = document.createElement("ha-panel-config");
+  await customElements.whenDefined('ha-panel-config');
+  const cpr = document.createElement('ha-panel-config');
   await cpr.routerOptions.routes.automation.load();
-}
+};
