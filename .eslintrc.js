5d4854d24ee79d584c8c967c6f42918db91af33a
Kendell R <KTibow@users.noreply.github.com>
2020-09-30 13:23:58 -0700

    Summary and remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
   extends: [
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:@typescript-eslint/recommended'  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
     ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
@@ -12,7 +10,6 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/camelcase": "off",
-    "prefer-const": "off",
-    "prettier/prettier": "off"
+    "prefer-const": "off"
   }
 };
