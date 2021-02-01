c18575f6d455063a386e3d3fc678d11130d10958
Kendell R <KTibow@users.noreply.github.com>
2021-01-31 18:37:56 -0800

    Create check.py

---
+++ .github/workflows/check.py
@@ -0,0 +1,27 @@
+# Setup
+import glob
+import json
+from colorama import init, Fore, Style
+
+init()
+# The thing
+for filename in glob.glob(r"./src/localize/languages/*.json"):
+    try:
+        json.load(open(filename, encoding="utf-8"))
+    except json.decoder.JSONDecodeError as e:
+        print(
+            "❗ The file",
+            Style.BRIGHT + filename,
+            Style.DIM + Fore.RED + "contains invalid JSON.",
+            Style.RESET_ALL + "Please fix it before merging this PR.",
+        )
+        print(
+            "You may need to add a",
+            Fore.GREEN + "comma at the end of a line" + Fore.WHITE,
+            "before adding another line.",
+        )
+        print(
+            "The error is",
+            Style.BRIGHT + Fore.RED + str(e).replace("Expecting", "expecting"),
+        )
+        raise e
