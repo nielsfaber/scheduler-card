754a01c09c08b973504fa1ad82225a109d4be6ae
Kendell R <KTibow@users.noreply.github.com>
2021-02-01 07:00:33 -0800

    Update check.py

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -6,19 +6,27 @@ from colorama import init, Fore, Style
 init()
 # Crossvalidator
 english_file = json.load(open("./src/localize/languages/en.json"))
+
+
 def cross_validate(english_value, other_language_value, other_language, key_name=None):
     if other_language_value is None:
-      print(f"⚠ In {other_language}, there is no value for {key_name}.")
+        print(f"⚠ In {other_language}, there is no value for {key_name}.")
     if type(english_value) != type(other_language_value):
-        raise Exception("The type of the English value and the type of {other_language}'s value are different.")
+        raise Exception(
+            f"The type of the English value ({english_value}) and the type of"
+            + f"{other_language}'s value ({other_language_value}) are different for key {key_name}."
+        )
     if isinstance(english_value, dict):
         for name, item in english_value.items():
             cross_validate(item, other_language_value.get(name), other_language, name)
-                           
+
+
 # The thing
 for filename in glob.glob("./src/localize/languages/*.json"):
     try:
-        cross_validate(english_file, json.load(open(filename, encoding="utf-8")), filename)
+        cross_validate(
+            english_file, json.load(open(filename, encoding="utf-8")), filename
+        )
     except json.decoder.JSONDecodeError as e:
         print(
             "❗ The file",
@@ -40,6 +48,5 @@ print(
     "🎉 All JSON files in",
     Style.BRIGHT + "src/localize/languages" + Style.DIM,
     "were validated",
-    Fore.GREEN
-    + "successfully."
+    Fore.GREEN + "successfully.",
 )
