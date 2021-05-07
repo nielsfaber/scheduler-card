79c77f0d7818812353757bf3521326181d3d14e3
niels <niels@pi.com>
2021-05-07 19:16:58 +0200

    update fan services

--- src/standard-configuration/fan.ts
+++ src/standard-configuration/fan.ts
@@ -1,108 +1,104 @@
 import { Action } from '../types';
-import { LocalizeFunc, HomeAssistant, computeStateDisplay } from 'custom-card-helpers';
+import { HomeAssistant, computeStateDisplay } from 'custom-card-helpers';
 import { HassEntity } from 'home-assistant-js-websocket';
 import { localize } from '../localize/localize';
 import { listVariable } from '../data/variables/list_variable';
+import { levelVariable } from '../data/variables/level_variable';
 
-const fanSpeeds = (localizeFunc: LocalizeFunc, stateObj?: HassEntity) => {
-  const speedList = [
+export const fanActions = (hass: HomeAssistant, stateObj?: HassEntity): Action[] => {
+  let actions: Action[] = [
     {
-      value: 'off',
-      icon: 'hass:fan-off',
-      name: localizeFunc('state.default.off').toLowerCase(),
+      service: 'fan.turn_on',
+      icon: 'hass:power',
+      name: hass.localize('ui.card.vacuum.actions.turn_on'),
     },
     {
-      value: 'low',
-      icon: 'hass:fan-speed-1',
-      name: localizeFunc('ui.card.climate.low').toLowerCase(),
+      service: 'fan.turn_off',
+      icon: 'hass:power-off',
+      name: hass.localize('ui.card.vacuum.actions.turn_off'),
     },
     {
-      value: 'medium',
-      icon: 'hass:fan-speed-2',
+      service: 'fan.set_percentage',
+      variables: {
+        percentage: levelVariable({
+          name: hass.localize('ui.card.fan.speed'),
+          min: 0,
+          max: 100,
+          step: 1,
+          unit: '%'
+        }),
+      },
+      supported_feature: 1,
+      icon: 'hass:weather-windy',
+      name: localize('services.fan.set_speed', hass.language),
     },
     {
-      value: 'high',
-      icon: 'hass:fan-speed-3',
-      name: localizeFunc('ui.card.climate.high').toLowerCase(),
+      service: 'fan.oscillate',
+      variables: {
+        oscillating: listVariable({
+          name: hass.localize('ui.card.fan.oscillate'),
+          options: [
+            {
+              value: 'True',
+              name: hass.localize('state.default.on'),
+              icon: 'hass:flash',
+            },
+            {
+              value: 'False',
+              name: hass.localize('state.default.off'),
+              icon: 'hass:flash-off',
+            }
+          ]
+        })
+      },
+      supported_feature: 2,
+      icon: 'hass:arrow-left-right',
+      name: localize('services.fan.oscillate', hass.language),
     },
+    {
+      service: 'fan.set_direction',
+      variables: {
+        direction: listVariable({
+          name: hass.localize('ui.card.fan.direction'),
+          options: [
+            {
+              value: 'forward',
+              name: hass.localize('ui.card.fan.forward'),
+              icon: 'hass:autorenew',
+            },
+            {
+              value: 'reverse',
+              name: hass.localize('ui.card.fan.reverse'),
+              icon: 'hass:sync',
+            },
+          ],
+        })
+      },
+      supported_feature: 4,
+      icon: 'hass:cog-clockwise',
+      name: localize('services.fan.set_direction', hass.language),
+    }
   ];
 
-  if (stateObj && stateObj.attributes.speed_list && Array.isArray(stateObj.attributes.speed_list)) {
-    return stateObj.attributes.speed_list.map(speed => speedList.find(el => el.value == speed) || { value: speed });
-  }
-  return speedList;
-};
+  const supportedPresets: string[] = stateObj && Array.isArray(stateObj.attributes.preset_modes)
+    ? stateObj.attributes.preset_modes
+    : [];
 
-export const fanActions = (hass: HomeAssistant, stateObj?: HassEntity): Action[] => [
-  {
-    service: 'fan.turn_on',
-    icon: 'hass:power',
-    name: hass.localize('ui.card.vacuum.actions.turn_on'),
-  },
-  {
-    service: 'fan.turn_off',
-    icon: 'hass:power-off',
-    name: hass.localize('ui.card.vacuum.actions.turn_off'),
-  },
-  {
-    service: 'fan.set_speed',
-    variables: {
-      speed: listVariable({
-        name: hass.localize('ui.card.fan.speed'),
-        options: fanSpeeds(hass.localize, stateObj),
-      }),
-    },
-    supported_feature: 1,
-    icon: 'hass:weather-windy',
-    name: localize('services.fan.set_speed', hass.language),
-  },
-  {
-    service: 'fan.oscillate',
-    variables: {
-      oscillating: listVariable({
-        name: hass.localize('ui.card.fan.oscillate'),
-        options: [
-          {
-            value: 'True',
-            name: hass.localize('state.default.on'),
-            icon: 'hass:flash',
-          },
-          {
-            value: 'False',
-            name: hass.localize('state.default.off'),
-            icon: 'hass:flash-off',
-          }
-        ]
-      })
-    },
-    supported_feature: 2,
-    icon: 'hass:arrow-left-right',
-    name: localize('services.fan.oscillate', hass.language),
-  },
-  {
-    service: 'fan.set_direction',
-    variables: {
-      direction: listVariable({
-        name: hass.localize('ui.card.fan.direction'),
-        options: [
-          {
-            value: 'forward',
-            name: hass.localize('ui.card.fan.forward'),
-            icon: 'hass:autorenw',
-          },
-          {
-            value: 'reverse',
-            name: hass.localize('ui.card.fan.reverse'),
-            icon: 'hass:sync',
-          },
-        ],
-      })
-    },
-    supported_feature: 4,
-    icon: 'hass:cog-clockwise',
-    name: localize('services.fan.set_direction', hass.language),
-  },
-];
+  if (supportedPresets.length)
+    actions.push({
+      service: 'fan.set_preset_mode',
+      variables: {
+        preset_mode: listVariable({
+          name: hass.localize('ui.card.fan.preset_mode'),
+          options: supportedPresets.map(e => Object({ value: e }))
+        })
+      },
+      supported_feature: 8,
+      icon: 'hass:cloud-download-outline',
+      name: localize('services.climate.set_preset_mode', hass.language),
+    });
+  return actions;
+};
 
 export const fanStates = (hass: HomeAssistant, stateObj: HassEntity) => listVariable({
   options: [
