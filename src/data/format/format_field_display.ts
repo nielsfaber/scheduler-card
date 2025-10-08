df347ec585648f8a9707fc18aceb4257cd3bd398
nielsfaber <mail@nielsfaber.com>
2025-10-08 17:39:46 +0200

    fix displayed name for custom variable

--- src/data/format/format_field_display.ts
+++ src/data/format/format_field_display.ts
@@ -3,6 +3,7 @@ import { HomeAssistant } from "../../lib/types";
 import { hassLocalize } from "../../localize/hassLocalize";
 import { Action, CustomConfig } from "../../types";
 import { compareActions } from "../actions/compare_actions";
+import { parseCustomActions } from "../actions/parse_custom_actions";
 
 
 
@@ -19,13 +20,17 @@ export const formatFieldDisplay = (action: Action, field: string, hass: HomeAssi
     hass.services[domain][action.service].fields[field]
   ) name = String(hass.services[domain][action.service].fields[field].name);
 
-  const match = Object.entries(customize || {})
-    .filter(([key]) => key == domain || key == action.service)
-    .map(([_key, e]) => (e.actions || []).filter(a => compareActions(a, action)))
-    .flat()
-    .find(e => Object.keys(e.variables || {}).includes(field));
-
-  if (match) name = match.variables![field].name || '';
+  const entityIds = ['script', 'notify'].includes(domain) ? [action.service] : [action.target?.entity_id || []].flat();
+  let actionConfig = parseCustomActions(customize || {}, entityIds.length ? entityIds[0] : domain);
+  if (actionConfig.length) {
+    let res = actionConfig.map(customConfig => {
+      if (customConfig.service != action.service || !Object.keys(customConfig.variables || {}).includes(field)) return null;
+      let variableConfig = (customConfig.variables || {})[field];
+      return variableConfig.name;
+    })
+      .filter(e => e !== undefined);
+    if (res.length) return res[0];
+  }
 
   if (!name) name = field.replace(/_/g, ' ');
 
