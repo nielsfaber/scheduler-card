96e7a4236ebc2b73bb0e2701a25d9d61d7aadfcd
nielsfaber <mail@nielsfaber.com>
2026-02-08 20:26:55 +0100

    fix transparent background in HA 2026.2

--- src/card.styles.ts
+++ src/card.styles.ts
@@ -5,43 +5,36 @@ export const EditorDialogStyles = css`
   ha-dialog {
     --mdc-dialog-min-width: 400px;
     --mdc-dialog-max-width: 600px;
-    --mdc-dialog-heading-ink-color: var(--primary-text-color);
-    --mdc-dialog-content-ink-color: var(--primary-text-color);
+    --mdc-dialog-max-width: min(600px, 95vw);
     --justify-action-buttons: space-between;
+    --dialog-container-padding: var(--safe-area-inset-top, 0)
+      var(--safe-area-inset-right, 0) var(--safe-area-inset-bottom, 0)
+      var(--safe-area-inset-left, 0);
+    --dialog-surface-padding: 0px;
     --dialog-content-padding: 0px;
   }
   /* make dialog fullscreen on small screens */
   @media all and (max-width: 450px), all and (max-height: 500px) {
     ha-dialog {
-      --mdc-dialog-min-width: calc(100vw - env(safe-area-inset-right) - env(safe-area-inset-left));
-      --mdc-dialog-max-width: calc(100vw - env(safe-area-inset-right) - env(safe-area-inset-left));
-      --mdc-dialog-min-height: 100%;
-      --mdc-dialog-max-height: 100%;
+      --mdc-dialog-min-width: 100vw;
+      --mdc-dialog-max-width: 100vw;
+      --mdc-dialog-min-height: 100vh;
+      --mdc-dialog-min-height: 100svh;
+      --mdc-dialog-max-height: 100vh;
+      --mdc-dialog-max-height: 100svh;
+      --dialog-container-padding: 0px;
+      --dialog-surface-padding: var(--safe-area-inset-top, 0)
+        var(--safe-area-inset-right, 0) var(--safe-area-inset-bottom, 0)
+        var(--safe-area-inset-left, 0);
       --vertical-align-dialog: flex-end;
-      --ha-dialog-border-radius: 0px;
+      --ha-dialog-border-radius: var(--ha-border-radius-square);
     }
   }
-  @media (min-width: 450px) and (min-height: 500px) {
-    .header-bar {
-        padding: 16px 16px 8px 16px;
-    }
-}
-  ha-dialog {
-    --dialog-surface-position: static;
-    --dialog-content-position: static;
-    --vertical-align-dialog: flex-start;
-  }
-  .content {
-    outline: none;
-  }
-  .heading {
-    border-bottom: 1px solid var(--mdc-dialog-scroll-divider-color, rgba(0, 0, 0, 0.12));
-  }
+
   @media all and (min-width: 600px) and (min-height: 501px) {
     ha-dialog {
-      --mdc-dialog-min-width: 560px;
+      --mdc-dialog-min-width: 580px;
       --mdc-dialog-max-width: 580px;
-      --dialog-surface-margin-top: 40px;
       --mdc-dialog-max-height: calc(100% - 72px);
     }
     :host([large]) ha-dialog {
